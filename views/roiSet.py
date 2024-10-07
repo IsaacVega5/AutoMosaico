d62e312d8deaad0caf7c284892d0adc5fe5ba02b
IsaacVega5 <isaacvega361@gmail.com>
2024-10-07 08:35:30 -0300

    [feat] Add origin image processing
    Se añade opción de incluir la imagen original en el procesamiento de roiSet, esta opción está desactivada por defecto.

--- views/roiSet.py
+++ views/roiSet.py
@@ -122,7 +122,7 @@ class roiSet(ctk.CTkFrame):
                        "¿De que forma desea exportar los datos?", 
                        EXPORT_TYPES,
                        checkVars={
-                         "¿Incluir imagen original?" : False,
+                         "Incluir imagen original" : False,
                          "¿Remover el suelo?" : False
                        })
     
@@ -151,15 +151,21 @@ class roiSet(ctk.CTkFrame):
       value_export = value_export.get()['alternatives']
     
     self.master.master.progressBar.start()
-    threading.Thread(target=self.generate, args=(export_form.get(),value_export)).start()
+    threading.Thread(target=self.generate, args=(export_form.get()['alternatives'],value_export, origin)).start()
       
     
-  def generate(self, export_form, value_export):
+  def generate(self, export_form, value_export, origin):
     workbook, destiny_path = create_workbook()
     if destiny_path is None: return
     progress_bar = self.master.master.progressBar
     progress_bar.lift(aboveThis=None)
-    values = process(roi_path=self.roi, img_roi_path=self.img, img_list = self.img_list, progress_bar = progress_bar)
+    img_list = self.img_list.copy()
+    if origin: img_list.insert(0, {
+        "path" : self.img,
+        "type" : self.type,
+      })
+    from pprint import pprint
+    values = process(roi_path=self.roi, img_roi_path=self.img, img_list = img_list, progress_bar = progress_bar)
     to_export = {
       'RGB' : [],
       'Termal' : [],
@@ -167,6 +173,7 @@ class roiSet(ctk.CTkFrame):
       'RGN' : [],
     }
     for i,value in enumerate(values):
+      pprint(value.keys())
       if value['values'] is False: 
         save_error(self.img_list, self.roi, self.img)
         msg = error(title='Error', message='Error al generar el xlsx\n\nVerifique que los tipos coincidan con las imágenes y vuelva a intentarlo.\nSi el problema persiste contacte con el administrador.', option_1 = "Cancelar", option_2 = "Continuar")
